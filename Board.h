5815a7e535c6e429d57d92b77c1b472d49e6bbc8
RaflyMuhamad <100673383+RaflyMuhamad@users.noreply.github.com>
2022-05-12 11:22:20 +0700

    Add files via upload

---
+++ Board.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+using std::vector;
+
+const int GRID_SIZE = 9;
+const int CELLS_TO_rEMOVE = 50;
+
+class Sudoku {
+private:
+	bool n_in_quad(int n, int i, int j);
+	bool n_in_col(int n, int j);
+
+	vector<bool> get_quad_possibles(int i, int j);
+	vector<bool> get_col_possibles(int j);
+	vector<bool> get_row_possibles(int i);
+
+	bool is_solution_right();
+	void copy_solution();
+
+	int grid[GRID_SIZE][GRID_SIZE];
+	int solution[GRID_SIZE][GRID_SIZE];
+	vector<bool> possibles[GRID_SIZE][GRID_SIZE];
+
+	int solution_changes;
+
+public :
+	Sudoku();
+	~Sudoku() = default;
+
+	void generate();
+	bool solve();
+	void draw();
+	void drawSolution();
+};

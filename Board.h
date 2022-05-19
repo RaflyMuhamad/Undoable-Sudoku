4753baddcc65ea8af77603fcf11e5132fd6b7c58
RaflyMuhamad <100673383+RaflyMuhamad@users.noreply.github.com>
2022-05-19 08:50:49 +0700

    Update

--- Board.h
+++ Board.h
@@ -1,39 +1,46 @@
 #pragma once
-#include <iostream>
-#include <random>
-#include <ctime>
-#include <vector>
+#include "Head.h"
+#define GRID_SIZE 9 //size for one region
 
-using namespace std;
-using std::vector;
 
-const int GRID_SIZE = 9;
-const int CELLS_TO_rEMOVE = 50;
-
-class Sudoku {
+class Board {
 private:
-	bool n_in_quad(int n, int i, int j);
+	bool n_in_region(int n, int i, int j);
 	bool n_in_col(int n, int j);
 
-	vector<bool> get_quad_possibles(int i, int j);
+	vector<bool> get_region_possibles(int i, int j);
 	vector<bool> get_col_possibles(int j);
 	vector<bool> get_row_possibles(int i);
 
 	bool is_solution_right();
 	void copy_solution();
 
+	void setCellSolved(int i, int j, int n);
+	int	 checkSolvedCells();
+	void checkRegion(int i, int j);
+	void copySol();
+
 	int grid[GRID_SIZE][GRID_SIZE];
 	int solution[GRID_SIZE][GRID_SIZE];
 	vector<bool> possibles[GRID_SIZE][GRID_SIZE];
 
+	int CELLS_TO_REMOVE = 50;
 	int solution_changes;
 
 public :
-	Sudoku();
-	~Sudoku() = default;
+	Board();
+	~Board() = default;
 
+	bool isSolRight();
+	void difficulty(int n);
 	void generate();
 	bool solve();
 	void draw();
 	void drawSolution();
+	int getCellSolValue(int x, int y);
+	int getCellValue(int x, int y);
+	void fillCell(int x, int y, int z);
+	void deleteCell(int x, int y);
+	void setCell(int x, int y, int z);
+	void setSolCell(int x, int y, int z);
 };
---
+++ Head.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <random>
+#include <ctime>
+#include <vector>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+
+/*contain header that will be used for this program*/
